b17c6263f9f40730d18870ea796042b20ad5655a
Pablo Roman <emanuelpabloroman@gmail.com>
2023-07-04 14:43:18 -0300

    feat: add the screen to manage the store

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return (
-  <div className={'bg-blue-900 w-screen h-screen flex flex items-center'}>
-    <div className="text-center w-full">
-      <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+  
+  const {data: session} = useSession();
+  console.log(session)
+  
+  if(!session) return;
+
+ return <Layout>
+  <div className=" text-blue-900 flex justify-between ">
+    <h2>hello, <b>{session?.user?.name}</b></h2>
+    <div className="flex bg-gray-300 text-black rounded-lg overflow-hidden">
+      <img src={session?.user?.image} className="w-6 h-6 "></img>
+
+      <span className="px-2">
+        {session?.user?.name}
+      </span>
     </div>
   </div>
-  )
+ </Layout>
 }
